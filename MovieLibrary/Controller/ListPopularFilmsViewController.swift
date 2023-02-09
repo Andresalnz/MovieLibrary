232d1590a0f0585f1d8cd859b839af94c8cc177e
AndresAleu <andresaleunz@gmail.com>
2023-02-09 16:24:36 +0100

    Add ViewController and Xib of the Home

---
+++ MovieLibrary/Controller/ListPopularFilmsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListPopularFilmsViewController.swift
+//  MovieLibrary
+//
+//  Created by Andres Aleu on 9/2/23.
+//
+
+import UIKit
+
+class ListPopularFilmsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
